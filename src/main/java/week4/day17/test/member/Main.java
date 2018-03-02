ff2bb41c3c07377f4954ab22d5c06d35d794e4bd
Raystar <raystar9@naver.com>
2018-03-02 21:45:17 +0900

    day17 test

--- src/main/java/week4/day17/test/member/Main.java
+++ src/main/java/week4/day17/test/member/Main.java
@@ -5,171 +5,190 @@ import java.util.Scanner;
 
 class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		ArrayList<MemberInfo> memberList = new ArrayList<>();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<MemberInfo> memberList = new ArrayList<>();
 
-		mainMenuLoop(sc, memberList);
+        mainMenuLoop(sc, memberList);
 
-		sc.close();
-	}
+        sc.close();
+    }
 
-	static void mainMenuLoop(Scanner sc, ArrayList<MemberInfo> memberList) {
-		String select;
-		int selector;
+    static void mainMenuLoop(Scanner sc, ArrayList<MemberInfo> memberList) {
+        String select;
+        int selector;
 
-		boolean menuQuit = false;
-		while (!menuQuit) {
+        boolean mainMenuRunning = true;
+        while (mainMenuRunning) {
 
-			Print.Menu.mainMenu();
-			select = sc.nextLine();
-			if (!isNumber(select)) {
-				Print.SystemMessage.notANumber(select);
-			} else {
-				selector = Integer.parseInt(select);
-				switch (selector) {
-				case 1:
-					inputInfo(memberList, sc);
-					break;
-				case 2:
-					searchMenu(memberList, sc);
-					break;
-				case 5:
-					Print.Menu.printAll(memberList);
-					break;
-				case 6:
-					Print.SystemMessage.quit();
-					menuQuit = true;
-					break;
-				default:
-					Print.SystemMessage.wrongNumber();
-				}
-			}
-		}
-	}
+            Print.Menu.mainMenu();
+            select = sc.nextLine();
+            if (!isNumber(select)) {
+                Print.SystemMessage.notANumber();
+            } else {
+                selector = Integer.parseInt(select);
+                switch (selector) {
+                    case 1:
+                        inputInfo(memberList, sc);
+                        break;
+                    case 2:
+                        searchMenu(memberList, sc);
+                        break;
+                    case 5:
+                        Print.Info.printAll(memberList);
+                        break;
+                    case 6:
+                        Print.SystemMessage.quit();
+                        mainMenuRunning = false;
+                        break;
+                    default:
+                        Print.SystemMessage.wrongNumber();
+                }
+            }
+        }
+    }
 
-	static void searchMenu(ArrayList<MemberInfo> memberList, Scanner sc) {
-		boolean searchMenuRunning = true;
-		int selector;
-		while (searchMenuRunning) {
-			Print.Menu.searchMenu();
-			String select = sc.nextLine();
-			if (!isNumber(select)) {
-				Print.SystemMessage.notANumber(select);
-			} else {
-				selector = Integer.parseInt(select);
-				switch (selector) {
-				case 1:
-					searchByName(memberList, sc);
-					break;
-				case 2:
-					searchMenu(memberList, sc);
-					break;
-				case 3:
-					Print.Menu.printAll(memberList);
-					break;
-				case 5:
-					Print.SystemMessage.returnMain();
-					searchMenuRunning = false;
-					break;
-				default:
-					Print.SystemMessage.wrongNumber();
-				}
-			}
-		}
-	}
-	
-	static void searchByName(ArrayList<MemberInfo> memberList, Scanner sc) {
-		Print.General.searchByName();
-		String str = sc.nextLine();
-		ArrayList<MemberInfo> results = SearchEngiene.byName(memberList, str);
-		if(results != null) {
-			Print.Menu.printAll(results);
-		} else {
-			Print.General.noResult();
-		}
-	}
-	
-	static void searchByAge(ArrayList<MemberInfo> memberList, Scanner sc) {
-		Print.General.searchByAge();
-		String str = sc.nextLine();
-		ArrayList<MemberInfo> results = SearchEngiene.byAge(memberList, str);
-		if(results != null) {
-			Print.Menu.printAll(results);
-		} else {
-			Print.General.noResult();
-		}
-	}
-	
-	static void searchByEMail(ArrayList<MemberInfo> memberList, Scanner sc) {
-		Print.General.searchByEMail();
-		String str = sc.nextLine();
-		ArrayList<MemberInfo> results = SearchEngiene.byEMail(memberList, str);
-		if(results != null) {
-			Print.Menu.printAll(results);
-		} else {
-			Print.General.noResult();
-		}
-	}
-	
-	static void searchByAddress(ArrayList<MemberInfo> memberList, Scanner sc) {
-		Print.General.searchByAddress();
-		String str = sc.nextLine();
-		ArrayList<MemberInfo> results = SearchEngiene.byAddress(memberList, str);
-		if(results != null) {
-			Print.Menu.printAll(results);
-		} else {
-			Print.General.noResult();
-		}
-	}
+    static void inputInfo(ArrayList<MemberInfo> memberList, Scanner sc) {
 
-	static void inputInfo(ArrayList<MemberInfo> memberList, Scanner sc) {
+        String name, email, address, ageStr;
+        int age;
+        boolean yq, isRunning;
+        MemberInfo member;
 
-		String name, email, address;
-		int age;
-		boolean yq, isRunning;
-		MemberInfo member = null;
+        isRunning = true;
+        while (isRunning) {
+            yq = false;
+            Print.General.inputName();
+            name = sc.nextLine();
+            while (true) {
+                Print.General.inputAge();
+                ageStr = sc.nextLine();
+                if (isNumber(ageStr)) {
+                    age = Integer.parseInt(ageStr);
+                    break;
+                }
+                Print.SystemMessage.notANumber();
+            }
+            Print.General.inputEMail();
+            email = sc.nextLine();
+            Print.General.inputAddress();
+            address = sc.nextLine();
 
-		isRunning = true;
-		while (isRunning) {
-			yq = false;
-			try {
-				System.out.print("Input name : ");
-				name = sc.nextLine();
-				System.out.print("Input age : ");
-				age = Integer.parseInt(sc.nextLine());
-				System.out.print("Input E-Mail : ");
-				email = sc.nextLine();
-				System.out.print("Input address : ");
-				address = sc.nextLine();
+            member = new MemberInfo(name, age, email, address);
 
-				member = new MemberInfo(name, age, email, address);
+            memberList.add(member);
 
-				memberList.add(member);
+            while (!yq) {
+                Print.General.yesOrQuit();
+                String in = sc.nextLine();
 
-				while (!yq) {
-					System.out.print("Input 'y' to continue, 'q' to quit : ");
-					String in = sc.nextLine();
+                if (in.equals("q")) {
+                    isRunning = false;
+                    Print.SystemMessage.returnToMain();
+                    yq = true;
+                } else if (in.equals("y")) {
+                    yq = true;
+                }
+            }
+        }
+    }
 
-					if (in.equals("q")) {
-						isRunning = false;
-						yq = true;
-					} else if (in.equals("y")) {
-						yq = true;
-					}
-				}
-			} catch (NumberFormatException e) {
-				System.out.println("error occurred");
-			}
-		}
-	}
+    static void searchMenu(ArrayList<MemberInfo> memberList, Scanner sc) {
+        boolean searchMenuRunning = true;
+        int selector;
+        while (searchMenuRunning) {
+            Print.Menu.searchMenu();
+            String select = sc.nextLine();
+            if (!isNumber(select)) {
+                Print.SystemMessage.notANumber();
+            } else {
+                selector = Integer.parseInt(select);
+                switch (selector) {
+                    case 1:
+                        searchByName(memberList, sc);
+                        break;
+                    case 2:
+                        searchByAge(memberList, sc);
+                        break;
+                    case 3:
+                        searchByEMail(memberList, sc);
+                        break;
+                    case 4:
+                        searchByAddress(memberList, sc);
+                        break;
+                    case 5:
+                        Print.SystemMessage.returnToMain();
+                        searchMenuRunning = false;
+                        break;
+                    default:
+                        Print.SystemMessage.wrongNumber();
+                }
+            }
+        }
+    }
 
-	static boolean isNumber(String str) {
-		try {
-			Integer.parseInt(str);
-			return true;
-		} catch (Exception e) {
-			return false;
-		}
-	}
+    //region search
+    static void searchByName(ArrayList<MemberInfo> memberList, Scanner sc) {
+        Print.General.inputName();
+        String str = sc.nextLine();
+        ArrayList<MemberInfo> results = SearchEngine.byName(memberList, str);
+        if (results != null) {
+            Print.Info.printResult(results);
+        } else {
+            Print.General.noResult();
+        }
+    }
+
+    static void searchByAge(ArrayList<MemberInfo> memberList, Scanner sc) {
+        String numStr;
+        int number;
+        while (true) {
+            Print.General.inputAge();
+            numStr = sc.nextLine();
+
+            if (isNumber(numStr)) {
+                number = Integer.parseInt(numStr);
+                break;
+            }
+        }
+
+        ArrayList<MemberInfo> results = SearchEngine.byAge(memberList, number);
+        if (results != null) {
+            Print.Info.printResult(results);
+        } else {
+            Print.General.noResult();
+        }
+    }
+
+    static void searchByEMail(ArrayList<MemberInfo> memberList, Scanner sc) {
+        Print.General.inputEMail();
+        String str = sc.nextLine();
+        ArrayList<MemberInfo> results = SearchEngine.byEMail(memberList, str);
+        if (results != null) {
+            Print.Info.printResult(results);
+        } else {
+            Print.General.noResult();
+        }
+    }
+
+    static void searchByAddress(ArrayList<MemberInfo> memberList, Scanner sc) {
+        Print.General.inputAddress();
+        String str = sc.nextLine();
+        ArrayList<MemberInfo> results = SearchEngine.byAddress(memberList, str);
+        if (results != null) {
+            Print.Info.printResult(results);
+        } else {
+            Print.General.noResult();
+        }
+    }
+    //endregion
+
+    static boolean isNumber(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) < '0' || str.charAt(i) > '9') {
+                return false;
+            }
+        }
+        return true;
+    }
 }
